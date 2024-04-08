b83fb825a53171898fcde3ca3a200863739a7ae0
islamhack01 <iaubakirov503@gmail.com>
2024-04-08 16:50:07 +0600

    .

---
+++ script.js
@@ -0,0 +1,232 @@
+// let name = "John";
+// let age = 20;
+// let price = 2000;
+
+// ("John");
+// 20;
+// 2000;
+
+//? let
+
+//? var
+
+//? const
+
+// let a = 1;
+// {
+//   a = 2;
+// }
+// console.log(a);
+
+// let sum = 0;
+// let arr = [1, 2, 3, 4, 5];
+// for (let i of arr) {
+//   sum += i;
+// }
+// console.log(sum);
+
+// {
+//   let a = 0;
+// }
+
+// console.log(a);
+
+// var a = 10;
+
+// var a = 20;
+
+// console.log(a);
+
+// let a = 1
+
+// a = 2
+
+// let Name =
+
+// let 1name =
+
+// let motion_web
+
+// let function name(params) {
+
+// }
+
+// let аты = "John";
+
+// console.log(аты);
+
+// str - ""
+// num - 9
+// boolean - true, false
+// undefined
+// let obj = {
+//   age: 20,
+// };
+
+// console.log(obj.age);
+// symb
+// null
+// bigInt
+
+// let s = Symbol("hgjhasgd");
+// console.log(typeof s);
+
+// let bigInt = 12345678903456789n;
+// console.log(bigInt);
+
+// let num;
+// console.log(num);
+
+// typeof
+
+// ? =
+
+// +
+
+// -
+
+// *
+
+// ==
+
+// ===
+
+// let num1 = "20";
+
+// let num2 = "3";
+
+// console.log(num1 / num2);
+
+// let a = "2";
+
+// let b = 2;
+
+// console.log(a === b);
+
+// / %
+
+// **
+
+// console.log(10 / 3);
+
+// console.log(2 ** 4);
+
+// jbjhj
+// jkjhkj
+
+/*
+jh  jkhjk
+
+jbjk
+
+
+kbj
+
+*/
+
+// alert("Hello World");
+// let a = prompt("введите число");
+// console.log(a);
+
+// let c = confirm();
+// console.log(c);
+
+// [
+//   {
+//     a: [{}],
+//   },
+// ];
+
+// [[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+
+// [[],[]]
+
+// [9, 1, 2, 3, 4];
+
+// let arr = [1, 2, 3, 4, 5, 3];
+
+// let res = arr.indexOf(3);
+
+// let res = arr.lastIndexOf(3);
+
+// console.log(res);
+// arr.unshift(0)
+
+// arr.push(6)
+
+// console.log(arr);
+
+// let arr = [1, 2, 3];
+
+// let str = arr.join(" ");
+
+// console.log(str);
+
+// includes() true, false
+
+// forEach((el) => {
+//   console.log(el);
+// });
+
+// let arr = [1, 2, 3, 4, 5];
+
+// let idx = arr.findIndex((el, index) => index == 3);
+
+// console.log(idx);
+
+// let newArr = arr.forEach((el) => el ** 2);
+
+// console.log(newArr);
+
+// let arr = [1, 2, 3, 4, 5];
+
+// for (let i of arr) {
+//   console.log(i * 2);
+// }
+
+// console.log(arr[0] * 2);
+// console.log(arr[1] * 2);
+// console.log(arr[2] * 2);
+// console.log(arr[3] * 2);
+// console.log(arr[4] * 2);
+
+// let i = 0;
+
+// i++ = i = i + 1
+
+// i-- = i = i - 1
+
+// for
+
+// let arr = [1, 2, 3, 4, 5];
+
+// for (let i = 0; i < arr.length; i++) {
+//   arr[i] % 2 === 0
+//     ? console.log(`${arr[i]} - четный`)
+//     : console.log("не четный");
+// }
+
+// for in
+
+// for of
+
+// while
+
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// let i = 1;
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i > 10);
+
+// do while
+
+//? for(let i of arr)
+
+//? for(let i in arr) - OBJECT
